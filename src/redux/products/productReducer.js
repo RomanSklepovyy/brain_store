6267dd61c92480a02f39f813653774ec0a769671
Roman <romansklepovyy1@gmail.com>
2020-12-11 13:21:50 +0200

    dataObject restructure

--- src/redux/products/productReducer.js
+++ src/redux/products/productReducer.js
@@ -1,7 +1,7 @@
 import * as types from './productsActionTypes';
 
 const initialState = {
-    products: [],
+    productsData: [],
     processing: false
 };
 
@@ -11,19 +11,73 @@ const productReducer = (state = initialState, action) => {
             console.log(action);
             return {
                 ...state,
-                products: action.result.data.items
+                productsData: createBooksObjectData(action.result.data)
                 }
             }
         case types.GET_PRODUCTS_BY_SEARCH_QUERY_SUCCESS: {
             console.log(action);
             return {
                 ...state,
-                products: action.result.data.items
+                productsData: createBooksObjectData(action.result.data)
             }
         }
         default:
             return state;
     }
 };
+
+const createBooksObjectData = (data) => {
+
+    try {
+        let newDataObject = {
+            //books remain in server
+            availableBooks: data.totalItems - data.items.length,
+            books: data.items.map(item => {
+
+                let {title, subtitle, authors, publisher, publishedDate, description, categories,
+                    averageRating, ratingsCount, language} = item.volumeInfo;
+
+                let {country, saleability} = item.saleInfo;
+
+                let saleDetails;
+
+                if (saleability === "FOR_SALE") {
+                    saleDetails = {
+                        price : item.saleInfo.listPrice.amount,
+                        currencyCode : item.saleInfo.listPrice.currencyCode,
+                        buyLink : item.saleInfo.buyLink
+                    }
+                }
+
+                return {
+                    id: item.id,
+                    title,
+                    subtitle,
+                    authors,
+                    publisher,
+                    publishedDate,
+                    description,
+                    categories,
+                    averageRating,
+                    ratingsCount,
+                    language,
+                    country,
+                    saleability,
+                    smallImage: (item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.smallThumbnail : null),
+                    normalImage: (item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : null),
+                    link: (item.volumeInfo.canonicalVolumeLink ? item.volumeInfo.canonicalVolumeLink : null),
+                    ...saleDetails
+                }
+            })
+        };
+
+        return newDataObject;
+
+    } catch (e) {
+        console.log('Error with creating books object data! \n', e);
+        return null;
+    }
+}
+
 
 export default productReducer;
